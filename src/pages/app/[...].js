59915ff7f670976ce0d22efc57000ee13301d1ff
Rachael Thomas <rachaelthomas@rachaels-mbp.lan>
2022-08-03 15:56:29 -0600

    Whoops - fixing the paths

--- src/pages/app/[...].js
+++ src/pages/app/[...].js
@@ -1,9 +1,9 @@
 import React from "react"
 import { Router } from "@reach/router"
-import Layout from "../components/layout"
-import Profile from "../components/profile"
-import Login from "../components/login"
-import PrivateRoute from "../components/privateRoute"
+import Layout from "../../components/layout"
+import Profile from "../../components/profile"
+import Login from "../../components/login"
+import PrivateRoute from "../../components/privateRoute"
 
 const App = () => {
   return (
